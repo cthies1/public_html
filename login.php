8c303a521983e6d2f6af7912cb3de5c2b870bc41
Chloe Thies <chloe.r.thies@gmail.com>
2021-11-17 14:06:01 -0800

    Update login.php

--- login.php
+++ login.php
@@ -4,32 +4,30 @@
         $db = new PDO('sqlite:./myDB/spoons.db');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-
-        if($_POST['username'] != '' && $_POST['password'] != '') {
-            $name = $_POST['username'];
-            $pass = md5($_POST['password']);
+        // if($_POST['username'] != '' && $_POST['password'] != '') {
+        //     $name = $_POST['username'];
+        //     $pass = md5($_POST['password']);
         
-            $query = mysql_query("SELECT * FROM `users` WHERE `email` = '$name' AND `password` = '$pass'");
+        //     $query = mysql_query("SELECT * FROM `users` WHERE `email` = '$name' AND `password` = '$pass'");
             
-            $rows = mysql_num_rows($query);
-            if($rows > 0) {
-                //successfull login
-                $_SESSION['username'] = $name;
-            } else {
-                $msg = "Invalid Login Credentials";
-            }
+        //     $rows = mysql_num_rows($query);
+        //     if($rows > 0) {
+        //         //successfull login
+        //         $_SESSION['username'] = $name;
+        //     } else {
+        //         $msg = "Invalid Login Credentials";
+        //     }
 
-                mysql_close($connection);
-        } else {
-            $msg = "Please Provide All Details";
-        }
+        //         mysql_close($connection);
+        // } else {
+        //     $msg = "Please Provide All Details";
+        // }
 
-
-        // $stmt = $db->prepare("SELECT * from User where Email is username;");
-        // $stmt->bindValue(':username',$_POST['username']);
-        // $result = $stmt->execute();
+        $stmt = $db->prepare("SELECT * from User where Email is username;");
+        $stmt->bindValue(':username',$_POST['username']);
+        $result = $stmt->execute();
         
-        // echo "Print: ".$result;
+        echo "Print: ".$result;
 
         //username is not associated with an account
         //if(mysql_num_rows($result) < 1) {//does this work???????? TODO
