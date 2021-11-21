d091f50972d1ce8c2f58f9f3570bd3a667cf1053
Beatrice Bridge <beatricembridge@gmail.com>
2021-11-21 13:10:01 -0800

    added = instead of is in SQL query

--- login.php
+++ login.php
@@ -26,7 +26,7 @@
             echo "database open";
 
             // //check email and passsword
-            $stmt = $db->prepare("SELECT * from Users where (Email is :email) and (Password is :pass)");
+            $stmt = $db->prepare("SELECT * from Users where (Email = :email) and (Password = :pass)");
             //$stmt = "SELECT * from Users where (Email is :email) and (Password is :pass) ;";
             //$stmt = "SELECT * from Users;";
             $email = $_POST['email'];
