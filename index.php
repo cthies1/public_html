baf4ba06d21413c7bd8c246eb769271e752d20fd
Chloe Thies <chloe.r.thies@gmail.com>
2021-11-29 14:53:08 -0800

    Update index.php

--- index.php
+++ index.php
@@ -37,13 +37,13 @@
             <form action="login.php?" method="post">
                 Email: <input type="email" name="email" /></br></br>
                 Password: <input type="text" name="pass" /></br></br>
-                <input type="submit" value="Login" /></br>
+                <input type="submit" value="Login" /></br></br>
             </form>
             <form action="SpoonsQuiz.php" method = "post">
-                <input type="submit" value="Spoon's quiz" /></br>
+                <input type="submit" value="Spoon's quiz" /></br></br>
             </form>
             <form action="createAccount.php" method = "post">
-                <input type="submit" value="Create Account" /></br>
+                <input type="submit" value="Create Account" /></br></br>
             </form>
         </p>
         
