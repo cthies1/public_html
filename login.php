29f207d6fe3f947db244400d75d746d9d2758f75
annavanboven <anna.van.boven@gmail.com>
2021-11-29 15:18:07 -0800

    err

--- login.php
+++ login.php
@@ -26,7 +26,7 @@
             echo "database open ...    ";
             echo "printing statement:   ";
             // //check email and password
-            $stmt = $db->prepare('SELECT * from Users where (Email = ":email") and (Password = ":pass")');
+            $stmt = $db->prepare('SELECT * from Users where (Email = :email) and (Password = :pass)');
             //$stmt = "SELECT * from Users where (Email is :email) and (Password is :pass) ;";
             //$stmt = "SELECT * from Users;";
             $email = $_POST['email'];
