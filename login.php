e4775100017bb96f6255ffa79371aa07b85654e6
Chloe Thies <chloe.r.thies@gmail.com>
2021-11-30 13:38:03 -0800

    Update login.php

--- login.php
+++ login.php
@@ -60,7 +60,7 @@
                         $numAttempts = 1;
                     }
                     $str = "Location: index.php?credentials=false&numAttempts=".$numAttempts;
-                    header($str);
+                    // header($str);
                 } else {
                     echo "else 2";
                     $str = "Location: index.php?credentials=false";
