904d5dd57b79242cc1845e0a2c00e4cd13936c8a
Chloe Thies <chloe.r.thies@gmail.com>
2021-11-30 13:09:50 -0800

    login

--- login.php
+++ login.php
@@ -27,24 +27,21 @@
             echo "printing statement:   ";
             // //check email and password
             $stmt = $db->prepare('SELECT * from Users where (Email = :email) and (Password = :pass)');
-            //$stmt = "SELECT * from Users where (Email is :email) and (Password is :pass) ;";
-            //$stmt = "SELECT * from Users;";
             $email = $_POST['email'];
             $password = $_POST['pass'];
             $stmt->bindValue(':email',$_POST['email']);
             $stmt->bindValue(':pass',$_POST['pass']);
-            //echo "statement prepared";
             
-            //$result = $db->query($stmt);
             $stmt->execute();
 
             $result = $stmt->fetchAll();
             echo "result";
-            //var_dump($result);
 
             if(is_Null($result[0]['fName'])){
+                $numAttempts = 
                 //echo "Invalid username or password.";
-                header("Location: index.php?credentials=false");
+                $str = "Location: index.php?credentials=".$numAttempts;
+                header($str);
                 
             }
             else {
@@ -64,17 +61,7 @@
                 //     echo "<td>".$result[0]['Email']."</td>";
                 //     echo "</tr>"; 
                
-            }
-             
-                //}
-            
-            //} 
-            // if ($result == null) {  //incorrect email or password
-            //     header("Location: login.php?credentials=false");
-            // } else {    //correct credentials, login
-            //     header("Location: homePage.php");
-            // }
-            
+            }    
         }
         //disconnect from database
         $db = null;
@@ -83,79 +70,4 @@
     {
         die('Exception : '.$e->getMessage());
     }
-
-
-/*
-    try {
-        //open the sqlite database file
-        $db_file = './assets/databases/spoons.db';
-        $db = new PDO('sqlite:' . $db_file);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        echo "Starting checks";
-
-        if(isset($_GET['username'])) {
-            $username = $_GET['username'];
-                echo "Username: ".$username;
-            $stmt = $db->prepare("SELECT * from Users where Email is $username;");
-            $result = $stmt->execute();
-            if($result == null) {   //is username associated with an account
-                header("Location: login.html?error=username");
-            } else {    //username exists now check password
-                if(isset($_GET['password'])) {
-                    $password = $_GET['password'];
-                        echo "Password: ".$password;
-                    $stmt = $db->prepare("SELECT * from Users where Password is $password;");
-                    $result = $stmt->execute();
-                } else {
-                    if($result == null) {   //password is incorrect
-                        header("Location: login.html?error=password");
-                    } else {    //password is correct
-                        header("Location: homePage.php");
-                    }
-                }
-            }
-        }
-        //disconnect from database
-        $db = null;
-
-    } catch(PDOException $e) {
-        die('Exception : '.$e->getMessage());
-    }
-*/
-
-
-
-
-/*        
-        if(isset($_GET['password'])) $password = $_GET['password'];
-        
-        //$stmt->bindValue(':username',$_POST['username']);
-        $result = $stmt->execute();
-        
-        echo "Print: ".$result;
-
-        //is username associated with an account
-        //if(mysql_num_rows($result) < 1) {//does this work???????? TODO
-        if($result == null) {
-            header("Location: login.html?error=username");
-        }
-
-        //username exists now check password
-        else {
-            $stmt =$db->prepare("SELECT Password from Users where Email is username;");
-            $stmt->bindValue(':username',$_POST['username']);
-            $result = $stmt->execute();
-            
-            //password is incorrect
-            if(strcmp($_POST['password'],$result) != 0) {
-                header("Location: login.html?error=password");
-            }
-
-            //password is correct
-            else {
-                header("Location: homePage.php");
-            }
-        } 
-*/ 
 ?>       
