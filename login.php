927a90b9489c9b27f48343ebea7ff48ea7f6315a
bbridge0200 <bbridge@pugetsound.edu>
2021-12-01 13:33:41 -0800

    Update login.php

--- login.php
+++ login.php
@@ -64,16 +64,16 @@
                         $numAttempts = 1;
                     }
                     $str = "Location: index.php?credentials=false&numAttempts=".$numAttempts;
-                    header($str);
+                    //header($str);
                 } else {
                     echo "else 2";
                     $str = "Location: index.php?credentials=false";
-                    header($str);
+                    //header($str);
                 }  
             } else {
                 echo "moving to home page...";
                 $str = "Location: homePage.php?username=".$_POST['email'];
-                header($str);
+                //header($str);
                 // echo "<table>";
                 // echo "<tr>";
                 //     echo "<th>fName</th><th>lName</th><th>email</th>";
