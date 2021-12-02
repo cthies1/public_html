51c624d96b0e494a63df7f93d6b5fba52e792f10
Chloe Thies <chloe.r.thies@gmail.com>
2021-12-01 17:53:57 -0800

    css

--- SpoonsQuiz.php
+++ SpoonsQuiz.php
@@ -15,16 +15,18 @@
         <div class="page">
             <h1>Welcome to Spoons!</h1>
             <h2> To get some matches, we need to know about you first! <br> Take this quiz to tell us about yourself!</h2>
-            <?php
-                if(isset($_GET["error"])){
-                        $err = $_GET["error"];
-                        
-                    if($err >= 10){
-                        echo "<font color='red'>*Hi, Sorry, you Must answer all Questions</br>";
-                        $err = $err-1;
+            <div class="error">
+                <?php
+                    if(isset($_GET["error"])){
+                            $err = $_GET["error"];
+                            
+                        if($err >= 10){
+                            echo "<font color='red'>*You must answer all questions!</br></br>";
+                            $err = $err-1;
+                        }
                     }
-                }
-            ?>
+                ?>
+            </div>
             <li>
                 <h3>What is your Sign?</h3>
                 <div>
@@ -358,6 +360,6 @@
             <br>
             <input class="button button1" type="submit" value="Submit" /></br></br>
         </form>
-    </div>
+    `</div>
 </body>
 </html>
