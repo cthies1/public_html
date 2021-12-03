99ffb1eb3a3cf45cbb2180666b7771380b4e25fe
annavanboven <anna.van.boven@gmail.com>
2021-12-03 14:51:20 -0800

    unmatch

--- generateReport.php
+++ generateReport.php
@@ -1,6 +1,8 @@
 <?php
     $user = $_GET['username'];
     $reporter = $_GET['reporter'];
+    $dFilt = $_GET['dFilt'];
+    $mFilt = $_GET['mFilt'];
     try{
         //open the sqlite database file
         $db_file = './assets/databases/spoons.db';
@@ -10,6 +12,16 @@
         $check->bindValue(':username',$user);
         $check->execute();
         $check = $check->fetchAll();
+
+        function dateToString($arr){
+            if($arr[2]==0){
+                return "00/00/0000";
+            }
+            return "{$arr[0]}/{$arr[1]}/{$arr[2]}";
+        }
+        $arr = getdate();
+        $tDate = array($arr['mon'],$arr['mday'],$arr['year']);
+        $tDate = dateToString($tDate);
         if($check){
             $numReports = $check[0]['numReports']+1;
             $quer = $db->prepare('update report set numReports=:nR where userID is :username;');
@@ -21,6 +33,19 @@
                 $quer2->bindValue(':username',$user);
                 $quer2->execute();
             }
+
+            $quer = $db->prepare('insert into unmatch values (:reporter, :user,:date);');
+            $quer->bindValue(':reporter',$reporter);
+            $quer->bindValue(':user',$username);
+            $quer->bindValue(':date',$tdate);
+            $quer->execute();
+
+            $quer = $db->prepare('insert into unmatch values (:user, :reporter, :date);');
+            $quer->bindValue(':reporter',$reporter);
+            $quer->bindValue(':user',$username);
+            $quer->bindValue(':date',$tdate);
+            $quer->execute();
+
             $str = "Location: homePage.php?username=".$reporter."&reported=1";
             header($str);
             exit;
@@ -29,6 +54,9 @@
             $quer = $db->prepare('insert into report values (:user, "mean",1);');
             $quer->bindValue(':user',$user);
             $quer->execute();
+            $str = "Location: homePage.php?username=".$reporter."&reported=1&dFilt=".$dFilt."&mFilt=".$mFilt;
+            header($str);
+            exit;
 
         }
 
---
+++ unMatch.php
@@ -0,0 +1,38 @@
+<?php
+    $user1 = $_GET['user1'];
+    $user2 = $_GET['user2'];
+    $dFilt = $_GET['dFilt'];
+    $mFilt = $_GET['mFilt'];
+    try{
+        //open the sqlite database file
+        $db_file = './assets/databases/spoons.db';
+        $db = new PDO('sqlite:' . $db_file);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        function dateToString($arr){
+            if($arr[2]==0){
+                return "00/00/0000";
+            }
+            return "{$arr[0]}/{$arr[1]}/{$arr[2]}";
+        }
+        $arr = getdate();
+        $tDate = array($arr['mon'],$arr['mday'],$arr['year']);
+        $tDate = dateToString($tDate);
+
+         $quer = $db->prepare('insert into unMatch values (:user1, :user2, :date);');
+         $quer->bindValue(':user1',$user1);
+         $quer->bindValue(':user2',$user2);
+         $quer->bindValue(':date',$tdate);
+         $quer->execute();
+         $str = "Location: homePage.php?username=".$user1."&reported=1&dFilt=".$dFilt."&mFilt=".$mFilt;
+          header($str);
+           exit;
+
+
+    }
+    catch(PDOException $e)
+    {
+        die('Exception : '.$e->getMessage());
+    }
+
+?>
