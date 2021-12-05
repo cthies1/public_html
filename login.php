0a17be4977136540c3123ff5c3b1e2547a485481
bbridge0200 <bbridge@pugetsound.edu>
2021-12-05 12:52:34 -0800

    uju

--- login.php
+++ login.php
@@ -19,113 +19,114 @@ session_start();
             session_destroy();
             // return to index
             header("Location: index.php");
-        }
+        } else {
         
-        $error = 0;
-        
-        if(null == ($_POST['email'])){  //email
-            $error += 10;
-        }
-        
-        if(null == ($_POST['pass'])){   //password
-            $error += 2;
-        }
-
-        if($error > 0) {
-            $str = "Location: index.php?error=".$error;
-            header($str);
-        }
-        else {
-
-            echo "Time to check credentials ";
-
-            //open the sqlite database file
-            $db_file = './assets/databases/spoons.db';
-            $db = new PDO('sqlite:' . $db_file);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            //check that email exists ONLY
-            $stmt1 = $db->prepare('SELECT * from Users where (Email = :email)');
-            $email = $_POST['email'];
-            $stmt1->bindValue(':email',$_POST['email']);
+            $error = 0;
             
-            $stmt1->execute();
-
-            $result1 = $stmt1->fetchAll();
-            // //check email and password
-            $stmt = $db->prepare('SELECT * from Users where (Email = :email) and (Password = :pass)');
-            $email = $_POST['email'];
-            $password = $_POST['pass'];
-            $stmt->bindValue(':email',$_POST['email']);
-            $stmt->bindValue(':pass',$_POST['pass']);
-            
-            $stmt->execute();
-
-            $result = $stmt->fetchAll();
-            //if email exists, but password is wrong
-            
-            if(!isset($result[0])){
-                //if email exists, but password is wrong
-            
-                if(isset($result1[0])){
-                    if(!isset($_GET["numAttempts"])){
-                        debug_to_console("inside if");
-                        $numAttempts = 1;
-                        
-                    } else {
-                        $numAttempts = $_GET["numAttempts"];
-                        $numAttempts = $numAttempts+1;
-                    }
-                    $str = "Location: index.php?credentials=false&numAttempts=".$numAttempts;
-                    header($str);
-                } else {
-                    echo "else 2";
-                    $str = "Location: index.php?credentials=false";
-                    header($str);
-                }  
-            } else {
-                echo "moving to home page...";
-                $_SESSION["email"] = $email;
-                if (isset($_POST["rememberMe"])){
-  
-                // Username is stored as cookie for 10 years as
-                // 10years * 365days * 24hrs * 60mins * 60secs
-                setcookie("email", $email, time() +
-                                    (60 * 60));
-  
-                // Password is stored as cookie for 10 years as 
-                // 10years * 365days * 24hrs * 60mins * 60secs
-                setcookie("password", $password, time() +
-                                    (60 * 60));
-  
-                // After setting cookies the session variable will be set
-                
-  
-            } else {
-                if (isset($_COOKIE["email"])){
-                    setcookie("email", "");
-                }
-                if (isset($_COOKIE["password"])){
-                    setcookie("password", "");
-                }
+            if(null == ($_POST['email'])){  //email
+                $error += 10;
             }
-                //$str = "Location: homePage.php?username=".$_POST['email'];
-                $str = "Location: homePage.php";
-                header($str);
-                // echo "<table>";
-                // echo "<tr>";
-                //     echo "<th>fName</th><th>lName</th><th>email</th>";
-                // echo "</tr>";
+            
+            if(null == ($_POST['pass'])){   //password
+                $error += 2;
+            }
 
-                // //while($arr = $stmt->fetchArray()) {
-                //     //foreach($result as $tuple) {
-                //     echo "<tr>";
-                //     echo "<td>".$result[0]['fName']."</td>";
-                //     echo "<td>".$result[0]['lName']."</td>";
-                //     echo "<td>".$result[0]['Email']."</td>";
-                //     echo "</tr>"; 
-               
-            }    
+            if($error > 0) {
+                $str = "Location: index.php?error=".$error;
+                header($str);
+            }
+            else {
+
+                echo "Time to check credentials ";
+
+                //open the sqlite database file
+                $db_file = './assets/databases/spoons.db';
+                $db = new PDO('sqlite:' . $db_file);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                //check that email exists ONLY
+                $stmt1 = $db->prepare('SELECT * from Users where (Email = :email)');
+                $email = $_POST['email'];
+                $stmt1->bindValue(':email',$_POST['email']);
+                
+                $stmt1->execute();
+
+                $result1 = $stmt1->fetchAll();
+                // //check email and password
+                $stmt = $db->prepare('SELECT * from Users where (Email = :email) and (Password = :pass)');
+                $email = $_POST['email'];
+                $password = $_POST['pass'];
+                $stmt->bindValue(':email',$_POST['email']);
+                $stmt->bindValue(':pass',$_POST['pass']);
+                
+                $stmt->execute();
+
+                $result = $stmt->fetchAll();
+                //if email exists, but password is wrong
+                
+                if(!isset($result[0])){
+                    //if email exists, but password is wrong
+                
+                    if(isset($result1[0])){
+                        if(!isset($_GET["numAttempts"])){
+                            debug_to_console("inside if");
+                            $numAttempts = 1;
+                            
+                        } else {
+                            $numAttempts = $_GET["numAttempts"];
+                            $numAttempts = $numAttempts+1;
+                        }
+                        $str = "Location: index.php?credentials=false&numAttempts=".$numAttempts;
+                        header($str);
+                    } else {
+                        echo "else 2";
+                        $str = "Location: index.php?credentials=false";
+                        header($str);
+                    }  
+                } else {
+                    echo "moving to home page...";
+                    $_SESSION["email"] = $email;
+                    if (isset($_POST["rememberMe"])){
+    
+                    // Username is stored as cookie for 10 years as
+                    // 10years * 365days * 24hrs * 60mins * 60secs
+                    setcookie("email", $email, time() +
+                                        (60 * 60));
+    
+                    // Password is stored as cookie for 10 years as 
+                    // 10years * 365days * 24hrs * 60mins * 60secs
+                    setcookie("password", $password, time() +
+                                        (60 * 60));
+    
+                    // After setting cookies the session variable will be set
+                    
+    
+                } else {
+                    if (isset($_COOKIE["email"])){
+                        setcookie("email", "");
+                    }
+                    if (isset($_COOKIE["password"])){
+                        setcookie("password", "");
+                    }
+                }
+                    //$str = "Location: homePage.php?username=".$_POST['email'];
+                    $str = "Location: homePage.php";
+                    header($str);
+                    // echo "<table>";
+                    // echo "<tr>";
+                    //     echo "<th>fName</th><th>lName</th><th>email</th>";
+                    // echo "</tr>";
+
+                    // //while($arr = $stmt->fetchArray()) {
+                    //     //foreach($result as $tuple) {
+                    //     echo "<tr>";
+                    //     echo "<td>".$result[0]['fName']."</td>";
+                    //     echo "<td>".$result[0]['lName']."</td>";
+                    //     echo "<td>".$result[0]['Email']."</td>";
+                    //     echo "</tr>"; 
+                
+                }    
+            }
         }
         //disconnect from database
         $db = null;
