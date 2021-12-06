b1e51a7beb2e5ea5800331c877524fa6d88c740f
Danielle Dolan <ddolan@pugetsound.edu>
2021-12-06 00:31:50 -0800

    change

--- login.php
+++ login.php
@@ -108,7 +108,7 @@ session_start();
                             $numAttempts = $numAttempts+1;
                         }
                         if($numAttempts >= 5){
-                            $str = "Location: resetPassword.php?username=".$_SESSION["email"];
+                            $str = "Location: resetPassword.php?username=".$_POST['email'];;
                             header($str);
                         }
                         $str = "Location: index.php?credentials=false&numAttempts=".$numAttempts;
