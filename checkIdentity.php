b64815074e56632214da25683fc19d522f43e7f1
Danielle Dolan <ddolan@pugetsound.edu>
2021-12-06 03:42:26 -0800

    check id

--- checkIdentity.php
+++ checkIdentity.php
@@ -11,22 +11,22 @@
         if(null == ($_POST['question'])){  
             $error += 10;
         }
+        // if errors send back
         if($error > 0) {
             $str = "Location: resetPassword.php?username=".$_SESSION["email"]."?error=".$error;
             header($str);
+            
         } else {
-            //$link = "checkIdentity.php?username=".$_SESSION["email"];
             $userID = $_SESSION["email"];
             //open the sqlite database file
             $db_file = './assets/databases/spoons.db';
             $db = new PDO('sqlite:' . $db_file);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            //get anwser in db for big or little spoon question
+            //get anwser for big or little spoon question
             $stmt = $db->prepare('SELECT response FROM Results WHERE QuizID = 1 and QuestionID = 2 and (UserID = :userID)');
             $userID = $_SESSION["email"];
             $stmt->bindValue(':userID', $userID);
-
             $stmt->execute();
 
             //get result
@@ -42,11 +42,14 @@
                     $stmt1->bindValue(':pass',$password);
                     $stmt1->execute();
 
-                    $link = "index.php";
-                    header($link);
+                    //disconnect from database
+                    $db = null;
                 }
             }
         }
+        // send back to login page
+        $str = "Location: index.php";
+        header($str);
     } catch(PDOException $e)
     {
         die('Exception : '.$e->getMessage());
