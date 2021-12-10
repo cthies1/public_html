33a5692f8a1ab150528f601720bbab473a910fb2
annavanboven <anna.van.boven@gmail.com>
2021-12-09 20:06:14 -0800

    dfg

--- userAnswers.php
+++ userAnswers.php
@@ -83,11 +83,7 @@ session_start();
                 }
             echo "</table>";
 
-            $quer = $db->prepare('delete from users where email is "bbridge@test.com"; ');
-            $quer->execute();
 
-            $quer = $db->prepare('delete from users where email is "avanboven@test.com"; ');
-            $quer->execute();
 
             $quer = $db->prepare("select userID from report where userID not in (select email from users);");
             //$quer = $db->prepare('select email as UserID from users where email is "bbridge@test.com" ;');
